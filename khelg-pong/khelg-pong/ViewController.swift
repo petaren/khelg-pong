426dac30410925f3a6c0e81b46c756c34fc117f9
Petar Mataic <petar@petar.se>
2015-02-23 13:51:02 +0100

    Initial commit with code

---
+++ khelg-pong/khelg-pong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  khelg-pong
+//
+//  Created by Petar Mataic on 23/02/15.
+//  Copyright (c) 2015 Jayway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
